abb0a968fd4b80c58bd9cd17c7f0514d9dd6dca5
Ahsan Habib <ahsanpub16b@gmail.com>
2022-05-29 17:13:40 +0600

    connent with mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
  
  
@@ -11,7 +12,18 @@ app.use(cors());
 app.use(express.json());
  
  
-//
+//connect to mongodb
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tb0uxuv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri);
+
+
+
+
+
+
  
 app.get('/', (req, res) => {
     res.send('Running X-part Manager Server');
